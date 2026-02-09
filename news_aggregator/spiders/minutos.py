b5a1fc297cf1b2e734068a0c7ebff1d00a357add
KarinaBaibak <karinabaibakk@gmail.com>
2026-02-09 16:16:18 +0100

    en vez de coger solo las 10 primera noticias coge todas
    Removed limit on the number of articles parsed from response.

--- news_aggregator/spiders/minutos.py
+++ news_aggregator/spiders/minutos.py
@@ -9,7 +9,7 @@ class VeinteMinutosSpider(scrapy.Spider):
         return text.strip() if text else ""
 
     def parse(self, response):
-        for article in response.css("article h2 a")[:10]:
+        for article in response.css("article h2 a"):
             title = self.clean(" ".join(article.css("::text").getall()))
             url = article.attrib.get("href")
 
